9ddcc1086265c70b17900c0daa52dbdd68e41a76
Lennon <lennonfusca@gmail.com>
2024-09-16 11:18:45 -0300

    correção

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,16 +3,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import * as VueGoogleMaps from 'vue2-google-maps'
+
 
 Vue.config.productionTip = false
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyCaJNUmCP8IWJbJ0vliXOXnPOq_0QYVaE0',
-    libraries: 'places', // Se você precisar da biblioteca de lugares
-  }
-})
+
 
 new Vue({
   vuetify,
